ae158f54707ef84d9ec1868d9b724678065bd692
Wyatt Rees <wyatt.rees@canonical.com>
2024-06-21 15:56:19 -0600

    Add prompt for ssl certificate and key for HAProxy, add to tf variables. Set tls config via settings yaml crts directory. Add instructions for TLS termination to README

--- anvil-python/anvil/commands/haproxy.py
+++ anvil-python/anvil/commands/haproxy.py
@@ -12,10 +12,11 @@
 # implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 import ipaddress
 import logging
-from typing import Any, List
+from os import environ, mkdir
+import os.path
+from typing import Any, ClassVar, List
 
 from rich.console import Console
 from sunbeam.clusterd.client import Client
@@ -36,21 +37,32 @@ LOG = logging.getLogger(__name__)
 APPLICATION = "haproxy"
 CONFIG_KEY = "TerraformVarsHaproxyPlan"
 HAPROXY_CONFIG_KEY = "TerraformVarsHaproxy"
-KEEPALIVED_CONFIG_KEY = "TerraformVarsKeepalived"
 HAPROXY_APP_TIMEOUT = 180  # 3 minutes, managing the application should be fast
 HAPROXY_UNIT_TIMEOUT = (
     1200  # 15 minutes, adding / removing units can take a long time
 )
+HAPROXY_CERTS_DIR = os.path.join(environ["HOME"], "certs/")
+LOG = logging.getLogger(__name__)
 
 
-def keepalived_questions() -> dict[str, questions.PromptQuestion]:
-    return {
-        "virtual_ip": questions.PromptQuestion(
-            "Virtual IP to use for the Cluster in HA",
-            default_value="",
-            validation_function=validate_virtual_ip,
-        ),
-    }
+def validate_cert_file(filepath: str | None) -> None:
+    if filepath is None:
+        return
+    if not os.path.isfile(filepath):
+        raise ValueError(f"{filepath} does not exist")
+    with open(filepath) as f:
+        if "BEGIN CERTIFICATE" not in f.read():
+            raise ValueError("Invalid certificate file")
+
+
+def validate_key_file(filepath: str | None) -> None:
+    if filepath is None:
+        return
+    if not os.path.isfile(filepath):
+        raise ValueError(f"{filepath} does not exist")
+    with open(filepath) as f:
+        if "BEGIN PRIVATE KEY" not in f.read():
+            raise ValueError("Invalid key file")
 
 
 def validate_virtual_ip(value: str) -> str:
@@ -63,10 +75,39 @@ def validate_virtual_ip(value: str) -> str:
         raise ValueError(f"{value} is not a valid IP address: {e}")
 
 
+def haproxy_questions() -> dict[str, questions.PromptQuestion]:
+    return {
+        "virtual_ip": questions.PromptQuestion(
+            "Virtual IP to use for the Cluster in HA",
+            default_value="",
+            validation_function=validate_virtual_ip,
+        ),
+        "ssl_cert": questions.PromptQuestion(
+            "Path to SSL Certificate for HAProxy (enter nothing to skip TLS)",
+            default_value=None,
+            validation_function=validate_cert_file,
+        ),
+        "ssl_key": questions.PromptQuestion(
+            "Path to private key for the SSL certificate (enter nothing to skip TLS)",
+            default_value=None,
+            validation_function=validate_key_file,
+        ),
+    }
+
+
 class DeployHAProxyApplicationStep(DeployMachineApplicationStep):
     """Deploy HAProxy application using Terraform"""
 
-    _KEEPALIVED_CONFIG = KEEPALIVED_CONFIG_KEY
+    _HAPROXY_CONFIG = HAPROXY_CONFIG_KEY
+    _TLS_SERVICES_CONFIG: str = f"""- service_name: haproxy_service
+  service_host: "0.0.0.0"
+  service_port: 443
+  service_options:
+    - balance leastconn
+    - cookie SRVNAME insert
+  server_options: maxconn 100 cookie S{"{i}"} check
+  crts: [{HAPROXY_CERTS_DIR}]
+"""
 
     def __init__(
         self,
@@ -92,6 +133,7 @@ class DeployHAProxyApplicationStep(DeployMachineApplicationStep):
         )
         self.preseed = deployment_preseed or {}
         self.accept_defaults = accept_defaults
+        self.variables: dict[str, Any] = {"charm_haproxy_config": {}}
 
     def get_application_timeout(self) -> int:
         return HAPROXY_APP_TIMEOUT
@@ -113,34 +155,55 @@ class DeployHAProxyApplicationStep(DeployMachineApplicationStep):
             return True
 
     def prompt(self, console: Console | None = None) -> None:
-        variables = questions.load_answers(
-            self.client, self._KEEPALIVED_CONFIG
-        )
+        variables = questions.load_answers(self.client, self._HAPROXY_CONFIG)
         variables.setdefault("virtual_ip", "")
+        variables.setdefault("charm_haproxy_config", {})
 
         # Set defaults
         self.preseed.setdefault("virtual_ip", "")
+        self.preseed.setdefault("charm_haproxy_config", {})
 
-        keepalived_config_bank = questions.QuestionBank(
-            questions=keepalived_questions(),
+        haproxy_config_bank = questions.QuestionBank(
+            questions=haproxy_questions(),
             console=console,
             preseed=self.preseed.get("haproxy"),
             previous_answers=variables,
             accept_defaults=self.accept_defaults,
         )
 
-        variables["virtual_ip"] = keepalived_config_bank.virtual_ip.ask()
+        variables["virtual_ip"] = haproxy_config_bank.virtual_ip.ask()
+
+        cert_filepath = haproxy_config_bank.ssl_cert.ask()
+        key_filepath = haproxy_config_bank.ssl_key.ask()
+        if cert_filepath is not None and key_filepath is not None:
+            with open(cert_filepath) as cert_file:
+                cert = cert_file.read()
+            with open(key_filepath) as key_file:
+                key = key_file.read()
+            if not os.path.isdir(HAPROXY_CERTS_DIR):
+                mkdir(HAPROXY_CERTS_DIR)
+            with open(
+                os.path.join(HAPROXY_CERTS_DIR, "haproxy.pem"), "w"
+            ) as combined_file:
+                combined_file.write(key + cert)
+            variables["charm_haproxy_config"]["services"] = (
+                self._TLS_SERVICES_CONFIG
+            )
+            variables["haproxy_port"] = 443
+        else:
+            LOG.debug(
+                "No certificate/key provided, skipping TLS configuration"
+            )
+            variables["haproxy_port"] = 80
 
         LOG.debug(variables)
-        questions.write_answers(
-            self.client, self._KEEPALIVED_CONFIG, variables
-        )
+        questions.write_answers(self.client, self._HAPROXY_CONFIG, variables)
 
     def extra_tfvars(self) -> dict[str, Any]:
         variables: dict[str, Any] = questions.load_answers(
-            self.client, self._KEEPALIVED_CONFIG
+            self.client, self._HAPROXY_CONFIG
         )
-        variables["haproxy_port"] = 80
+        LOG.debug(f"extra tfvars: {variables}")
         return variables
 
 
