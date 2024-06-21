18d5c1b0932b29b18e5dbad886e30b694fc96503
Wyatt Rees <wyatt.rees@canonical.com>
2024-06-21 15:56:19 -0600

    Add prompt for ssl certificate and key for HAProxy, add to tf variables

--- anvil-python/anvil/commands/haproxy.py
+++ anvil-python/anvil/commands/haproxy.py
@@ -12,11 +12,14 @@
 # implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import os.path
+import logging
 from typing import List
 
+from rich.console import Console
 from sunbeam.clusterd.client import Client
 from sunbeam.commands.terraform import TerraformInitStep
+from sunbeam.jobs import questions
 from sunbeam.jobs.juju import JujuHelper
 from sunbeam.jobs.manifest import BaseStep
 from sunbeam.jobs.steps import (
@@ -30,11 +33,22 @@ from anvil.provider.local.deployment import LocalDeployment
 
 APPLICATION = "haproxy"
 CONFIG_KEY = "TerraformVarsHaproxyPlan"
+ADDONS_CONFIG_KEY = "TerraformVarsHaproxyAddons"
 HAPROXY_APP_TIMEOUT = 180  # 3 minutes, managing the application should be fast
 HAPROXY_UNIT_TIMEOUT = (
     1200  # 15 minutes, adding / removing units can take a long time
 )
-
+HAPROXY_ADDONS_QUESTIONS = {
+    "ssl_cert": questions.PromptQuestion(
+        "Path to SSL Certificate for HAProxy: ",
+        validation_function=os.path.isfile, # TODO: should we do any other validation?
+    ),
+    "ssl_key": questions.PromptQuestion(
+        "Path to private key for the SSL certificate",
+        validation_function=os.path.isfile,
+    ),
+}
+LOG = logging.getLogger(__name__)
 
 class DeployHAProxyApplicationStep(DeployMachineApplicationStep):
     """Deploy HAProxy application using Terraform"""
@@ -59,9 +73,36 @@ class DeployHAProxyApplicationStep(DeployMachineApplicationStep):
             "Deploying HAProxy",
             refresh,
         )
+        self.variables = {"addons": {}}
 
     def get_application_timeout(self) -> int:
         return HAPROXY_APP_TIMEOUT
+
+    def has_prompts(self) -> bool:
+        # maybe return False if it's a refresh?
+        return True
+
+    def prompt(self, console: Console | None = None) -> None:
+        haproxy_addons_bank = questions.QuestionBank(
+            questions=HAPROXY_ADDONS_QUESTIONS,
+            console=console,
+        )
+        with open(haproxy_addons_bank.ssl_cert.ask(), "r") as cert_file:
+            self.variables["addons"]["ssl_cert"] = cert_file.read()
+        with open(haproxy_addons_bank.ssl_key.ask(), "r") as key_file:
+            self.variables["addons"]["ssl_key"] = key_file.read()
+
+        LOG.debug(f"HAProxy addon variables: {self.variables}")
+
+        # TODO: do we need this? probably for refresh?
+        questions.write_answers(self.client, ADDONS_CONFIG_KEY, self.variables)
+
+        tfhelper = self.manifest.get_tfhelper(self.tfplan)
+        answer_file = tfhelper.path / "addons.auto.tfvars.json"
+        tfhelper.write_tfvars(self.variables, answer_file)
+
+    def extra_tfvars(self) -> dict:
+        return self.variables
 
 
 class AddHAProxyUnitsStep(AddMachineUnitsStep):
