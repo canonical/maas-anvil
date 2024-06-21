59d0fdadd39a051c367954219e9888b15da6e806
Jack Lloyd-Walters <jack.lloyd-walters@canonical.com>
2024-06-21 15:27:16 +0100

    provider passing

--- cloud/etc/deploy-haproxy/main.tf
+++ cloud/etc/deploy-haproxy/main.tf
@@ -70,6 +70,6 @@ resource "juju_integration" "haproxy_keepalived" {
   count = length(var.virtual_ip) > 0 ? 1 : 0
 
   model    = data.juju_model.machine_model.name
-  provider = juju_application.haproxy.name
+  provider = juju.haproxy
   requirer = juju_application.keepalived.name
 }
