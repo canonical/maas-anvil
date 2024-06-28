39d02b1f6f755fdb5d7c77d5ded0633d27e89b72
Wyatt Rees <wyatt.rees@canonical.com>
2024-06-28 14:33:00 -0600

    set tls via settings yaml with crts directory instead of passing ssl_cert and ssl_key config vars

--- anvil-python/anvil/commands/haproxy.py
+++ anvil-python/anvil/commands/haproxy.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import os.path
+from os import environ, mkdir
 import logging
 from typing import List
 
@@ -38,20 +39,57 @@ HAPROXY_APP_TIMEOUT = 180  # 3 minutes, managing the application should be fast
 HAPROXY_UNIT_TIMEOUT = (
     1200  # 15 minutes, adding / removing units can take a long time
 )
-HAPROXY_ADDONS_QUESTIONS = {
-    "ssl_cert": questions.PromptQuestion(
-        "Path to SSL Certificate for HAProxy: ",
-        validation_function=os.path.isfile, # TODO: should we do any other validation?
-    ),
-    "ssl_key": questions.PromptQuestion(
-        "Path to private key for the SSL certificate",
-        validation_function=os.path.isfile,
-    ),
-}
+HAPROXY_CERTS_DIR = os.path.join(environ['HOME'], "certs/")
 LOG = logging.getLogger(__name__)
+
+
+def validate_cert_file(filepath: str) -> None:
+    if not os.path.isfile(filepath):
+        raise ValueError(f"{filepath} does not exist")
+    try:
+        with open(filepath, "r") as f:
+            parse_cert(f.read())
+    except:
+        raise ValueError("Invalid certificate file format")
+
+def validate_key_file(filepath: str) -> None:
+    if not os.path.isfile(filepath):
+        raise ValueError(f"{filepath} does not exist")
+    try:
+        with open(filepath, "r") as f:
+            parse_key(f.read())
+    except:
+        raise ValueError("Invalid key file format")
+
+def parse_cert(cert: str) -> str:
+    return '\n'.join([x for x in cert.split("\n") if x and "CERTIFICATE" not in x])
+
+def parse_key(key: str) -> str:
+    return '\n'.join([x for x in key.split("\n") if x and "KEY" not in x])
+
 
 class DeployHAProxyApplicationStep(DeployMachineApplicationStep):
     """Deploy HAProxy application using Terraform"""
+
+    _HAPROXY_ADDONS_QUESTIONS = {
+        "ssl_cert": questions.PromptQuestion(
+            "Path to SSL Certificate for HAProxy: ",
+            validation_function=validate_cert_file,
+        ),
+        "ssl_key": questions.PromptQuestion(
+            "Path to private key for the SSL certificate",
+            validation_function=validate_key_file,
+        ),
+    }
+    _TLS_SERVICES_CONFIG = f"""- service_name: haproxy_service
+  service_host: "0.0.0.0"
+  service_port: 443
+  service_options:
+    - balance leastconn
+    - cookie SRVNAME insert
+  server_options: maxconn 100 cookie S{"{i}"} check
+  crts: [{HAPROXY_CERTS_DIR}]
+"""
 
     def __init__(
         self,
@@ -73,7 +111,7 @@ class DeployHAProxyApplicationStep(DeployMachineApplicationStep):
             "Deploying HAProxy",
             refresh,
         )
-        self.variables = {"addons": {}}
+        self.variables = {"charm_haproxy_config": {}}
 
     def get_application_timeout(self) -> int:
         return HAPROXY_APP_TIMEOUT
@@ -84,15 +122,20 @@ class DeployHAProxyApplicationStep(DeployMachineApplicationStep):
 
     def prompt(self, console: Console | None = None) -> None:
         haproxy_addons_bank = questions.QuestionBank(
-            questions=HAPROXY_ADDONS_QUESTIONS,
+            questions=self._HAPROXY_ADDONS_QUESTIONS,
             console=console,
         )
         with open(haproxy_addons_bank.ssl_cert.ask(), "r") as cert_file:
-            self.variables["addons"]["ssl_cert"] = cert_file.read()
+            cert = cert_file.read()
         with open(haproxy_addons_bank.ssl_key.ask(), "r") as key_file:
-            self.variables["addons"]["ssl_key"] = key_file.read()
-
-        LOG.debug(f"HAProxy addon variables: {self.variables}")
+            key = key_file.read()
+        if not os.path.isdir(HAPROXY_CERTS_DIR):
+            mkdir(HAPROXY_CERTS_DIR)
+        with open(os.path.join(HAPROXY_CERTS_DIR, "haproxy.pem"), "w") as combined_file:
+            combined_file.write(key + cert)
+        
+        self.variables["charm_haproxy_config"]["services"] = self._TLS_SERVICES_CONFIG
+        LOG.debug(f"HAProxy prompt variables: {self.variables}")
 
         # TODO: do we need this? probably for refresh?
         questions.write_answers(self.client, ADDONS_CONFIG_KEY, self.variables)
