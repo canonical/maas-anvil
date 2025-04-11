62430b56489b88913f2b75ff657effef321148dc
Stamatis Katsaounis <stamatis.katsaounis@canonical.com>
2025-04-11 18:41:26 +0300

    replace addgroup with usermod

--- anvil-python/anvil/commands/prepare_node.py
+++ anvil-python/anvil/commands/prepare_node.py
@@ -61,7 +61,7 @@ sudo snap connect maas-anvil:ssh-keys
 
 # Add $USER to the snap_daemon group supporting interaction
 # with the MAAS Anvil clustering daemon for cluster operations.
-sudo addgroup $USER snap_daemon
+sudo usermod --append --groups snap_daemon $USER
 
 # Generate keypair and set-up prompt-less access to local machine
 [ -f $HOME/.ssh/id_rsa ] || ssh-keygen -b 4096 -f $HOME/.ssh/id_rsa -t rsa -N ""
