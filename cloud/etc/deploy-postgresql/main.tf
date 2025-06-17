741ef5f4db0fd351ce23636f6a820bb3ceebbf3c
Jack Lloyd-Walters <jack.lloyd-walters@canonical.com>
2025-06-17 14:53:13 +0100

    add vars

--- cloud/etc/deploy-postgresql/main.tf
+++ cloud/etc/deploy-postgresql/main.tf
@@ -63,3 +63,82 @@ resource "juju_application" "postgresql" {
     "arch=${var.arch}",
   ])
 }
+
+resource "juju_application" "s3_integrator" {
+  name      = "s3-integrator"
+  model     = data.juju_model.machine_model.name
+  units     = 3
+  placement = ["0", "1", "2"]
+
+  charm {
+    name    = "s3-integrator"
+    channel = var.charm_s3_integrator_channel
+  }
+
+  config = merge(
+    {
+      "endpoint" = "https://s3.${var.aws_region}.amazonaws.com"
+      "bucket"   = var.aws_bucket
+      "path"     = "/postgresql"
+      "region"   = var.aws_region
+    },
+    var.charm_s3_integrator_config,
+  )
+
+  constraints = "arch=${var.arch}"
+}
+
+resource "terraform_data" "juju_wait_for_s3_postgres" {
+  input = {
+    model = juju_application.s3_integrator.model
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      juju wait-for model "$MODEL" --timeout 3600s \
+        --query='
+            all(
+                filter(
+                    units,
+                    unit => startsWith(unit.application.name, "s3-integrator") || startsWith(unit.application.name, "postgresql")
+                ),
+            unit => unit.workload-status == "active"
+        )'
+    EOT
+    environment = {
+      MODEL = self.input.model
+    }
+  }
+}
+
+resource "terraform_data" "sync_credentials" {
+  input = {
+    model = terraform_data.juju_wait_for_s3_postgres.model
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+        juju run s3-integrator/leader sync-s3-credentials \
+            access-key="${AWS_ACCESS_KEY}" \
+            secret-key="${AWS_SECRET_KEY}"
+    EOT
+    environment = {
+      AWS_ACCESS_KEY = var.aws_access_key
+      AWS_SECRET_KEY = var.aws_secret_key
+    }
+  }
+}
+
+resource "juju_integration" "postgresql_s3_integration" {
+  model = terraform_data.sync_credentials.model
+
+  application {
+    name     = "s3-integrator"
+    endpoint = "s3"
+  }
+
+  application {
+    name     = "postgresql"
+    endpoint = "database"
+  }
+}
