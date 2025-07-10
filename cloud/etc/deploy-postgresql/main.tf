0104b6f14f9fb39cb55a122383efb213024ebddf
Jack Skye Lloyd-Walters <jack.lloyd-walters@canonical.com>
2025-07-10 13:04:06 +0100

    feat: Add postgres s3 backup (#99)

--- cloud/etc/deploy-postgresql/main.tf
+++ cloud/etc/deploy-postgresql/main.tf
@@ -38,6 +38,18 @@ locals {
       experimental_max_connections = tonumber(var.max_connections)
     }
   )
+
+  s3_config = merge(
+    {
+      "endpoint" = coalesce(var.endpoint, "https://s3.${var.region}.amazonaws.com")
+      "bucket"   = var.bucket
+      "path"     = "/postgresql"
+      "region"   = var.region
+    },
+    var.charm_s3_integrator_config,
+  )
+
+  s3_enabled = var.s3_enabled ? 1 : 0
 }
 
 resource "juju_application" "postgresql" {
@@ -62,4 +74,66 @@ resource "juju_application" "postgresql" {
   constraints = join(" ", [
     "arch=${var.arch}",
   ])
+}
+
+
+resource "juju_secret" "s3_credentials" {
+  count = local.s3_enabled
+
+  model = data.juju_model.machine_model.name
+  name  = "s3_credentials"
+
+  value = {
+    "access-key" = var.access_key
+    "secret-key" = var.secret_key
+  }
+  info = "Credentials used to access S3"
+}
+
+resource "juju_application" "s3_integrator" {
+  count = local.s3_enabled
+
+  name  = "s3-integrator"
+  model = data.juju_model.machine_model.name
+
+  # TODO: This one should go away when we move out of manual cloud
+  placement = "0"
+
+  charm {
+    name     = "s3-integrator"
+    channel  = var.charm_s3_integrator_channel
+    revision = var.charm_s3_integrator_revision
+    base     = "ubuntu@24.04"
+  }
+
+  config = merge(
+    local.s3_config,
+    { "credentials" = "secret:${juju_secret.s3_credentials[0].secret_id}" },
+  )
+
+  constraints = "arch=${var.arch}"
+}
+
+resource "juju_access_secret" "s3_credentials" {
+  count = local.s3_enabled
+
+  model        = data.juju_model.machine_model.name
+  applications = [juju_application.s3_integrator[0].name]
+  secret_id    = juju_secret.s3_credentials[0].secret_id
+}
+
+resource "juju_integration" "postgresql_s3_integration" {
+  count = local.s3_enabled
+
+  model = data.juju_model.machine_model.name
+
+  application {
+    name     = juju_application.s3_integrator[0].name
+    endpoint = "s3-credentials"
+  }
+
+  application {
+    name     = juju_application.postgresql.name
+    endpoint = "s3-parameters"
+  }
 }
